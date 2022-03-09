392932ff9290f67aba45fd836c9902ecf3d78bd5
Yusuf Abiç <yusuf.abic@outlook.com>
2022-03-09 17:39:11 +0300

    redux installation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store";
 
 render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById("root")
 );
