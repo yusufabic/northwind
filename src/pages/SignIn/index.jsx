348a74470b18ac1a71779e87145757899bf8247b
Yusuf Abiç <yusuf.abic@outlook.com>
2022-06-09 13:26:10 +0300

    validations and error messages

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import { Link, useHistory } from "react-router-dom";
 import { signIn } from "../../config/firebase";
 
+const SigInValidations = Yup.object().shape({
+  email: Yup.string().required("Required field!"),
+  password: Yup.string().required("Required field!").min(6, "Min value 6!"),
+});
+
 const SignIn = ({ setIsLoggedIn }) => {
+  const [errorMessage, setErrorMessage] = useState("");
   const history = useHistory();
   return (
     <div className="container m-5">
@@ -11,6 +19,7 @@ const SignIn = ({ setIsLoggedIn }) => {
           email: "",
           password: "",
         }}
+        validationSchema={SigInValidations}
         onSubmit={(values) => {
           signIn(values.email, values.password)
             .then(() => {
@@ -19,7 +28,8 @@ const SignIn = ({ setIsLoggedIn }) => {
             })
             .catch((e) => {
               // TODO: Hata mesajının ekranda gözükmesi sağlanacak
-              console.log("e", e.message);
+              setErrorMessage(e.message);
+              console.log("YA ~ e.message", e.message);
             });
         }}
       >
@@ -29,11 +39,20 @@ const SignIn = ({ setIsLoggedIn }) => {
 
             <div>Email</div>
             <Field name="email" type="email" className="form-control" />
-            <div className="error"></div>
+            <div className="error">
+              {errors.email && touched.email ? (
+                <div className="text-danger text-sm">{errors.email}</div>
+              ) : null}
+            </div>
 
             <div className="mt-2">Password</div>
             <Field name="password" type="password" className="form-control" />
-            <div className="error"></div>
+            <div className="error">
+              {errors.password && touched.password ? (
+                <div className="text-danger text-sm">{errors.password}</div>
+              ) : null}
+            </div>
+            <div className="mt-5 text-danger">{errorMessage}</div>
 
             <button type="submit" className="btn btn-primary mt-4">
               Sign In
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import { signUp } from "../../config/firebase";
 
+const SignUpValidations = Yup.object().shape({
+  email: Yup.string()
+    .required("Required field!")
+    .email("Please enter an email"),
+  password: Yup.string().required("Required field!").min(6, "Min value 6!"),
+});
 const SignUp = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   return (
     <div className="container m-5">
       <Formik
@@ -9,13 +18,12 @@ const SignUp = () => {
           email: "",
           password: "",
         }}
+        validationSchema={SignUpValidations}
         onSubmit={(values) => {
           signUp(values.email, values.password)
-            .then((response) => {
-              console.log("YA ~ response", response);
-            })
+            .then()
             .catch((e) => {
-              console.log(e);
+              setErrorMessage(e.message);
             });
         }}
       >
@@ -25,11 +33,20 @@ const SignUp = () => {
 
             <div>Email</div>
             <Field name="email" type="email" className="form-control" />
-            <div className="error"></div>
+            <div className="error">
+              {errors.email && touched.email ? (
+                <div className="text-danger text-sm">{errors.email}</div>
+              ) : null}
+            </div>
 
             <div className="mt-2">Password</div>
             <Field name="password" type="password" className="form-control" />
-            <div className="error"></div>
+            <div className="error">
+              {errors.password && touched.password ? (
+                <div className="text-danger text-sm">{errors.password}</div>
+              ) : null}
+            </div>
+            <div className="mt-5 text-danger">{errorMessage}</div>
 
             <button type="submit" className="btn btn-primary mt-4">
               Sign Up
